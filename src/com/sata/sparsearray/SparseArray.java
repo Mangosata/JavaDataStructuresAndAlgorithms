bf207b7be73fcf57ec4097a7f451068b8da8130b
binjia <mangosata@gmail.com>
2020-07-09 20:34:58 -0400

    SparseArray

--- src/com/sata/sparsearray/SparseArray.java
+++ src/com/sata/sparsearray/SparseArray.java
@@ -9,7 +9,7 @@ public class SparseArray {
   public static void main(String[] args) {
     // 创建一个原始的二维数组 11 * 11
     // 0：表示没有棋子，1表示黑子，2表示蓝子
-    int chessArr1[][] = new int[11][11];
+    int[][] chessArr1 = new int[11][11];
     chessArr1[1][2] = 1;
     chessArr1[2][3] = 2;
     chessArr1[4][5] = 2;
@@ -32,7 +32,7 @@ public class SparseArray {
       }
     }
     // 2. 创建对应的稀疏数组
-    int sparseArr[][] = new int[sum + 1][3];
+    int[][] sparseArr = new int[sum + 1][3];
     // 给稀疏数组赋值
     sparseArr[0][0] = 11;
     sparseArr[0][1] = 11;
@@ -54,13 +54,13 @@ public class SparseArray {
     // 输出稀疏数组的形式
     System.out.println();
     System.out.println("稀疏数组为:");
-    for (int i = 0; i < sparseArr.length; i++) {
-      System.out.printf("%d\t%d\t%d\t\n", sparseArr[i][0], sparseArr[i][1], sparseArr[i][2]);
+    for (int[] ints : sparseArr) {
+      System.out.printf("%d\t%d\t%d\t\n", ints[0], ints[1], ints[2]);
     }
 
     // 将稀疏数组恢复为原始的二维数组
     // 1. 根据稀疏数组第一行数据，创建原始的二维数组
-    int chessArr2[][] = new int[sparseArr[0][0]][sparseArr[0][1]];
+    int[][] chessArr2 = new int[sparseArr[0][0]][sparseArr[0][1]];
 
     // 2. 读取稀疏数组的剩余数据，从第二行开始，赋值给原始二维数组
     for (int i = 1; i < sparseArr.length; i++) {
